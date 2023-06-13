b3d49681c8ab6879e47b7a8043c2fdbbb7274dca
Li Zhanhui <lizhanhui@gmail.com>
2023-06-13 11:40:20 +0000

    Package rocksdb deb
    Signed-off-by: Li Zhanhui <lizhanhui@gmail.com>

---
+++ dist/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+BASEDIR=$(dirname "$0")
+cd "$BASEDIR/.." || exit 1
+make shared_lib -j $(nproc) || exit 1
+cp -L librocksdb.so dist/rocksdb/usr/local/lib/
+cp -r include/rocksdb dist/rocksdb/usr/local/include/
+cd dist
+strip rocksdb/usr/local/lib/librocksdb.so
+rm rocksdb/usr/local/lib/.gitignore
+rm rocksdb/usr/local/include/.gitignore
+dpkg-deb -b rocksdb
