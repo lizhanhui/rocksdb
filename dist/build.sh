a7b4c845948345e8fb905ddcf9a5c29919ab885f
Li Zhanhui <lizhanhui@gmail.com>
2023-06-13 23:30:03 +0800

    Fix cp of shared libraries with links
    Signed-off-by: Li Zhanhui <lizhanhui@gmail.com>

--- dist/build.sh
+++ dist/build.sh
@@ -2,10 +2,10 @@
 BASEDIR=$(dirname "$0")
 cd "$BASEDIR/.." || exit 1
 make shared_lib -j $(nproc) || exit 1
-cp -L librocksdb.so dist/rocksdb/usr/local/lib/
+cp --no-dereference --preserve=links librocksdb.so* dist/rocksdb/usr/local/lib/
 cp -r include/rocksdb dist/rocksdb/usr/local/include/
 cd dist
-strip rocksdb/usr/local/lib/librocksdb.so
+strip -g rocksdb/usr/local/lib/librocksdb.so
 rm rocksdb/usr/local/lib/.gitignore
 rm rocksdb/usr/local/include/.gitignore
 dpkg-deb -b rocksdb
