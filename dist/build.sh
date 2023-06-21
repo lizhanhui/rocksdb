1c636f5667c3efae011c1688f5747f8608db5e73
Li Zhanhui <lizhanhui@gmail.com>
2023-06-21 07:36:27 +0000

    Fix Architecture field if host is aarch64

--- dist/build.sh
+++ dist/build.sh
@@ -8,4 +8,9 @@ cd dist
 strip -g rocksdb/usr/local/lib/librocksdb.so
 rm rocksdb/usr/local/lib/.gitignore
 rm rocksdb/usr/local/include/.gitignore
+arch=$(uname -m)
+if [ "$arch" == "aarch64" ]; then
+  echo "Change architecture in control file"
+  sed -i 's/amd64/arm64/g' rocksdb/DEBIAN/control
+fi
 dpkg-deb -Zxz -b rocksdb
